63008c45cbed360ce19de2bff145a0ac8b43a55b
abdelhamidsbai <36006224+abdelhamidsbai@users.noreply.github.com>
2020-11-01 23:19:31 +0100

    Ajoutez des fichiers projet.

---
+++ Inbooster/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Inbooster.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Inbooster.Areas.Identity.IdentityHostingStartup))]
+namespace Inbooster.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
